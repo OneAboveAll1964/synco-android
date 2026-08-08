34806efb53f419916d075f6eee53320a1d23baa2
OneAboveAll1964 <shkoma.ranya@gmail.com>
2026-08-08 13:17:50 +0300

    Bump to 1.0.3

--- build-logic/convention/src/main/kotlin/app/synco/buildlogic/BuildConstants.kt
+++ build-logic/convention/src/main/kotlin/app/synco/buildlogic/BuildConstants.kt
@@ -7,8 +7,8 @@ object BuildConstants {
     const val MIN_SDK = 29
     const val TARGET_SDK = 36
     const val APPLICATION_ID = "com.shkomaghdid.synco.android"
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.0.2"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.0.3"
     val JAVA_VERSION: JavaVersion = JavaVersion.VERSION_17
     const val JVM_TARGET = 17
 }
